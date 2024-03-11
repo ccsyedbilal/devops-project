d717fb7441fb2f5b27db1d18b3ba8cdbe2788a01
Syed Bilal Chand <109179646+csyedbilal@users.noreply.github.com>
2024-03-12 01:09:32 +0530

    Delete syed-travel directory

--- syed-travel/src/providers/toaster.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Toaster } from "@/components/ui/toaster";
-import { ReactElement } from "react";
-
-export default function ToasterProviders(): ReactElement {
-  return <Toaster />;
-}
